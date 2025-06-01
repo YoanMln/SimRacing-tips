93d082b3c2e2f96f7f9c86cc84324881069cae00
YoanMln <yoan.moulin@hotmail.fr>
2025-06-01 11:50:59 +0200

    Configuration Layout header

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rity SimRacing tips </title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+    <div class="main-container">
+        <div class="nav-container">
+        <nav>
+            <div class="logo-container">
+                <img class="logo" src="#" alt="logo du site">
+            </div>
+            <ul>
+                <li> <a href="index.php?pages=home">Accueil</a></li>
+                <li><a href="index.php?pages=settings">Réglages</a>  </li>
+                <li> <a href="index.php?pages=tips">Astuces</a> </li>
+            </ul>
+        </nav>
+        </div>
+        
+        
+  
+    
