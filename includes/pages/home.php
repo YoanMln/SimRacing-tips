c564c97a3efb532c7c716337f58d2c3192ef8d00
YoanMln <yoan.moulin@hotmail.fr>
2025-06-01 16:08:04 +0200

    Modification h2

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,4 +1,12 @@
+<header>
 <div class="title-container">
             <h1 class="title">Rity SimRacing tips</h1>
             </div>
-        <h2 class="intro">Ici, vous trouverez les meilleurs réglages pour vos voitures, des conseils pour améliorer vos chronos et les dernières vidéos de course</h2>
+        <h2 class="intro">Ici, vous trouverez des réglages pour vos voitures, des conseils pour améliorer vos chronos et les dernières vidéos de course</h2>
+</header>
+<main>
+    <div class="video-container">
+<h3 class="video-title">Dernière vidéo YouTube</h3>
+<iframe width="460" height="270" src="https://www.youtube.com/embed/uzIQ00pMips?si=2cyzaLdbvssDI1Hs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+</main>
