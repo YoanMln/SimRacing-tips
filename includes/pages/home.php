2655db7b79f09c869b3873570fcc5e1be052d1dd
YoanMln <yoan.moulin@hotmail.fr>
2025-06-01 13:17:05 +0200

    Modification h2

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,4 +1,4 @@
 <div class="title-container">
             <h1 class="title">Rity SimRacing tips</h1>
-            <h2 class="bio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus vero recusandae cumque ea perspiciatis magnam excepturi animi similique temporibus!</h2>
-        </div>
+            </div>
+        <h2 class="bio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus vero recusandae cumque ea perspiciatis magnam excepturi animi similique temporibus!</h2>
