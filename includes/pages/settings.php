9aa20cb7a78d73e167c857337c1ccee9da061eb6
YoanMln <yoan.moulin@hotmail.fr>
2025-06-03 12:25:10 +0200

    Ajout form + select choix circuit

--- includes/pages/settings.php
+++ includes/pages/settings.php
@@ -2,6 +2,17 @@
     <div class="container-title">
     <h1>Réglages des voitures</h1>
     </div>
+    <form action="index.php?pages=settings" method="GET">
+    <div class="track-container">
+        
+        <label  for="track-choice">Choix du circuit</label><br>
+        <select name="track" id="track-choice" onchange="this.form.submit()">
+            <option value="Nurburgring">Nurburgring</option>
+            <option value="Spa-Francorchamps">Spa-Francorchamps</option>
+            <option value="RedBull Ring">RedBull Ring</option>
+        </select>
+    </div>
+    </form>
     <section class="car-settings">
         <div class="container-card">
     <?php
@@ -16,6 +27,7 @@
             echo '<li><strong>Pression pneus :</strong> ' . htmlspecialchars($car['tyre_pressure']) . '</li>';
             echo '<li><strong>pneus :</strong> ' . htmlspecialchars($car['tyres']) . '</li>';
             echo '<li><strong>Carrossage :</strong> ' . htmlspecialchars($car['camber']) . '</li>';
+            echo '<li><strong>Circuit : </strong>' .htmlspecialchars($car['circuit']) . '</li>';
             echo '<li><strong>Notes :</strong> ' . htmlspecialchars($car['notes']) . '</li>';
             echo '</ul>';
             echo '</article>';
