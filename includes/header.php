919bc3049c14fe441bf49d44f05530db67152fb8
YoanMln <yoan.moulin@hotmail.fr>
2025-06-03 15:22:17 +0200

    Ajout logo

--- includes/header.php
+++ includes/header.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SimRacing tips </title>
     <link rel="stylesheet" href="/assets/CSS/style.css">
     <link rel="stylesheet" href="/assets/CSS/colors.css">
-    <?php if (file_exists ("assets/CSS/{$pages}.css")) : ?>
-        <link rel="stylesheet" href="assets/css/<?=$pages ?>.css">
+    <?php if (file_exists("assets/CSS/{$pages}.css")) : ?>
+        <link rel="stylesheet" href="assets/css/<?= $pages ?>.css">
     <?php endif; ?>
 </head>
+
 <body>
     <div class="main-container">
         <div class="nav-container">
-        <nav>
-            <div class="logo-container">
-                <img class="logo" src="#" alt="logo du site">
-            </div>
-            <ul>
-                <li> <a href="index.php?pages=home">Accueil</a></li>
-                <li><a href="index.php?pages=settings">Réglages</a>  </li>
-                <li> <a href="index.php?pages=tips">Astuces</a> </li>
-            </ul>
-        </nav>
+            <nav>
+                <div class="logo-container">
+                    <img class="logo" src="../assets/images/logo/logo-site.png" alt="logo du site">
+                </div>
+                <ul>
+                    <li> <a href="index.php?pages=home">Accueil</a></li>
+                    <li><a href="index.php?pages=settings">Réglages</a> </li>
+                    <li> <a href="index.php?pages=tips">Astuces</a> </li>
+                </ul>
+            </nav>
         </div>
-        
-        
-  
-    
