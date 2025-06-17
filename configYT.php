6c25be16800f5be1e4a820bdcebf492f13b08a36
YoanMln <yoan.moulin@hotmail.fr>
2025-06-17 09:47:48 +0200

    Ajout API YouTube, fichier .env

---
+++ configYT.php
@@ -0,0 +1,11 @@
+<?php
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$apiKey = $_ENV['apiKey'];
+$channelId = $_ENV['channelId'];
--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,12 +1,37 @@
 <header>
-<div class="title-container">
-            <h1 class="title">SimRacing tips</h1>
-            </div>
-        <h2 class="intro">Ici, vous trouverez des réglages pour vos voitures, des conseils pour améliorer vos chronos et les dernières vidéos de course</h2>
+    <div class="title-container">
+        <h1 class="title">SimRacing tips</h1>
+    </div>
+    <h2 class="intro">Ici, vous trouverez des réglages pour vos voitures, des conseils pour améliorer vos chronos et les dernières vidéos de course</h2>
 </header>
 <main>
     <div class="video-container">
-<h3 class="video-title">Dernière vidéo YouTube</h3>
-<iframe width="460" height="270" src="https://www.youtube.com/embed/uzIQ00pMips?si=2cyzaLdbvssDI1Hs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
+        <h3 class="video-title">Dernière vidéo YouTube</h3>
+        <?php
+        require_once 'configYT.php';
+
+        //  Récupére la playlist d’uploads de la chaîne
+        $channelUrl = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=$channelId&key=$apiKey";
+        $channelData = json_decode(file_get_contents($channelUrl), true);
+
+        if (isset($channelData['items'][0]['contentDetails']['relatedPlaylists']['uploads'])) {
+            $uploadPlaylistId = $channelData['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
+
+            //  Récupére la dernière vidéo
+            $videosUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=$uploadPlaylistId&maxResults=1&key=$apiKey";
+            $videosData = json_decode(file_get_contents($videosUrl), true);
+
+            if (isset($videosData['items'][0]['snippet']['resourceId']['videoId'])) {
+                $videoId = $videosData['items'][0]['snippet']['resourceId']['videoId'];
+
+                // Affichage  vidéo
+                echo "<iframe width='560' height='315' src='https://www.youtube.com/embed/$videoId' frameborder='0' allowfullscreen></iframe>";
+            } else {
+                echo "<p>Impossible de récupérer la dernière vidéo.</p>";
+            }
+        } else {
+            echo "<p>La playlist de la chaîne n’a pas été trouvée.</p>";
+        }
+        ?>
+    </div>
 </main>
